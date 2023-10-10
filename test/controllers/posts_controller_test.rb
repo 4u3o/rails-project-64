63f1d5d7881e901f2c6babac8fa06065518c024e
Anton Goroshko <radjahair@gmail.com>
2023-10-10 11:13:05 +1000

    feat: add posts_controller tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,48 +1,47 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @post = posts(:one)
   end
 
-  test "should get index" do
-    get posts_url
+  test "#index" do
+    get root_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "#new" do
+    sign_in users(:one)
+
     get new_post_url
     assert_response :success
   end
 
-  test "should create post" do
+  test "#create" do
+    sign_in users(:one)
+
     assert_difference("Post.count") do
-      post posts_url, params: { post: { body: @post.body, category_id: @post.category_id, creator_id: @post.creator_id, title: @post.title } }
+      post posts_url,
+           params: {
+             post: {
+               body: @post.body,
+               category_id: @post.category_id,
+               title: @post.title
+             }
+           }
     end
+
+    assert { Post.last.title == @post.title }
+    assert { Post.last.body == @post.body }
+    assert { Post.last.category_id == @post.category_id }
 
     assert_redirected_to post_url(Post.last)
   end
 
-  test "should show post" do
+  test "#show" do
     get post_url(@post)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_post_url(@post)
-    assert_response :success
-  end
-
-  test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, category_id: @post.category_id, creator_id: @post.creator_id, title: @post.title } }
-    assert_redirected_to post_url(@post)
-  end
-
-  test "should destroy post" do
-    assert_difference("Post.count", -1) do
-      delete post_url(@post)
-    end
-
-    assert_redirected_to posts_url
   end
 end
