e9f001359eba2d0b89ae7e0cce8316d6c2c5b20d
Anton Goroshko <radjahair@gmail.com>
2023-10-12 20:06:55 +1000

    fix: linting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   belongs_to :category
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   validates :title, :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
                            inverse_of: :creator, dependent: :destroy
   has_many :comments, class_name: 'Post::Comment', foreign_key: :author_id,
                       inverse_of: :author, dependent: :destroy
-  has_many :likes, class_name: 'Post::Like', inverse_of: :user
+  has_many :likes, class_name: 'Post::Like', inverse_of: :user, dependent: :destroy
 
   def liked_post?(post)
     likes.find_by(post_id: post.id).present?
